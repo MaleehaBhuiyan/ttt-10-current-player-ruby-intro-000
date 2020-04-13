70525f0805fb7880810a0c0b590d1e2869b47233
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-04-13 17:14:27 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,8 @@
 board = [" ", " ", " "," ", " ", " "," ", " ", " "]
 def turn_count(board}
   counter = 1
-  board_array.each do |index|
-    if index == "X" || index == "O"
+  board_array.each do |space|
+    if space == "X" || space == "O"
       counter += 1
     end
 end
