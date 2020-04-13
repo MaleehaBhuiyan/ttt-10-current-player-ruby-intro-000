69c7c2dedf376c3217540c7023d40e3298dfc017
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-04-13 17:11:34 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,10 @@
 
-counter = 1
 def turn_count(board_array}
+  counter = 1
   board_array.each do |index|
-    if board_array[index] == "X" || "O"
+    if index == "X" || index == "O"
       counter += 1
     end
+end
+return counter
 end
